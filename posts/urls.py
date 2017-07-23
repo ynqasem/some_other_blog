457292884b3675ec78075ff90d9a3cf09fd23533
DarthHamza <hamzamakia@gmail.com>
2017-07-23 20:39:20 +0300

    created a list and detail page that are linked

--- posts/urls.py
+++ posts/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    
-    url(r'^post_stuff/$', views.post, name="stuff"),
+    url(r'^list/$', views.post_list, name="list"),
+    url(r'^detail/(?P<post_id>\d+)/$', views.post_detail, name="detail"),
 ]
--- posts/views.py
+++ posts/views.py
@@ -2,9 +2,18 @@ from django.shortcuts import render
 from .models import Post
 from django.shortcuts import get_object_or_404
 
-def post(request):
-	obj = get_object_or_404(Post, id=1000000)
+
+def post_list(request):
+	obj_list = Post.objects.all()
+	context = {
+		"post_list": obj_list,
+	}
+	return render(request, 'post_list.html', context)
+
+
+def post_detail(request, post_id):
+	obj = get_object_or_404(Post, id=post_id)
 	context = {
 		"instance": obj,
 	}
-	return render(request, 'whatever.html', context)
+	return render(request, 'post_detail.html', context)
