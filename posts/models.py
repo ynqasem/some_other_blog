61fecf97a23ecf245a683ebbc37d4eb55a32601f
DarthHamza <hamzamakia@gmail.com>
2017-07-25 18:46:28 +0300

    Create, update, delete and some awesome messages

--- posts/models.py
+++ posts/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
 class Post(models.Model):
 	title = models.CharField(max_length=100)
@@ -8,3 +9,6 @@ class Post(models.Model):
 
 	def __str__(self):
 		return self.title
+
+	def get_absolute_url(self):
+		return reverse("posts:detail", kwargs={"post_id": self.id})
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Post
+from .forms import PostForm
 from django.shortcuts import get_object_or_404
+from django.contrib import messages
 
 
 def post_list(request):
@@ -17,3 +19,35 @@ def post_detail(request, post_id):
 		"instance": obj,
 	}
 	return render(request, 'post_detail.html', context)
+
+def post_create(request):
+	form = PostForm(request.POST or None)
+	if form.is_valid():
+		form.save()
+		messages.success(request, "OMG! So cool! You created an object.")
+		return redirect("posts:list")
+	context = {
+		"form":form,
+	}
+	return render(request, 'post_create.html', context)
+
+
+def post_update(request, post_id):
+	post_object = get_object_or_404(Post, id=post_id)
+	form = PostForm(request.POST or None, instance=post_object)
+	if form.is_valid():
+		form.save()
+		messages.success(request, "Giving it a second thought?")
+		return redirect("posts:list")
+	context = {
+		"form":form,
+		"post_object":post_object,
+	}
+	return render(request, 'post_update.html', context)
+
+
+def post_delete(request, post_id):
+	Post.objects.get(id=post_id).delete()
+	messages.warning(request, "Seriously bro?")
+	return redirect("posts:list")
+	
